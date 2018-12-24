92e8984cf4c644fbd001e4a078b113eae6150f19
Wassim Akachi <wassim@akachiw.comm>
2018-12-24 01:13:57 +0100

    add solution for exercise_03-02

---
+++ exercise_03-02/list.erl
@@ -0,0 +1,8 @@
+-module(list).
+-export([create/1, reverse_create/1]).
+
+create(N) when N >= 0 -> create_acc(N, []).
+reverse_create(N) when N >= 0 -> lists:reverse(create(N)).
+
+create_acc(0, List) -> List;
+create_acc(N, List) -> create_acc(N - 1, [N | List]).
