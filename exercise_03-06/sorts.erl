21669615bb1cf3a7e661652f634da4a6f4a6e9a3
Wassim Akachi <wassim@akachiw.comm>
2018-12-27 10:07:53 +0100

    add solution for exercise_03-06

--- exercise_03-06/sorts.erl
+++ exercise_03-06/sorts.erl
@@ -1,5 +1,5 @@
 -module(sorts).
--export([quicksort/1]).
+-export([quicksort/1, mergesort/1]).
 
 quicksort([]) -> [];
 quicksort([OneElement]) -> [OneElement];
@@ -28,3 +28,35 @@ partition(Pivot, AllSmaller, AllGreater, [First|Rest]) when First < Pivot ->
 %% than add the element to the greater collection
 partition(Pivot, AllSmaller, AllGreater, [First|Rest]) when First >= Pivot -> 
   partition(Pivot, AllSmaller, [First|AllGreater], Rest).
+
+
+%% 
+%% MergeSort
+mergesort([]) -> [];
+mergesort([OneElement]) -> [OneElement];
+
+mergesort(List) -> 
+  {Left, Right} = split_in_two(List),
+  SortedLeft = mergesort(Left),
+  SortedRight = mergesort(Right),
+  megre(SortedLeft, SortedRight).
+
+%% Split the array in two parts
+split_in_two(List) -> split_in_two(List, [],[]).
+
+split_in_two([], Left, Right) -> {Left, Right};
+split_in_two([H|T], Left, Right) -> split_in_two(T, Right, [H|Left]).
+
+megre(SortedLeft, SortedRight) -> merge(SortedLeft, SortedRight, []).
+
+merge([], [], Result) -> Result;
+merge([], Right, Result) -> Result ++ Right;
+merge(Left, [], Result) -> Result ++ Left;
+
+merge([HeadLeft|Left], [HeadRight|Right], Result) -> 
+  if
+    HeadLeft < HeadRight -> merge(Left, [HeadRight|Right], append(Result, [HeadLeft]));
+    true -> merge([HeadLeft|Left], Right, append(Result, [HeadRight]))
+  end.
+
+append(ListA, ListB) -> ListA ++ ListB.
