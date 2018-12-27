4f686e8d43f880ffedd0520e4d6ec6b39ad3b878
Wassim Akachi <wassim@akachiw.comm>
2018-12-27 22:00:00 +0100

    add solutions to exercise_03-08

--- exercise_03-08/pretty_printer.erl
+++ exercise_03-08/pretty_printer.erl
@@ -1,11 +1,11 @@
--module(parser).
+-module(pretty_printer).
 -export([print/1]).
 
 %% A pretty printer, which will turn an exp into a string representation
-print(Expression) -> lists:flatten(pretty_printer_1(Expression)).
+print(Expression) -> lists:flatten(pretty_printer(Expression)).
 
-pretty_printer_1({num, Num}) -> io_lib:format("~B", [Num]);
-pretty_printer_1({minus, A, B}) -> 
-  io_lib:format("(~s - ~s)", [pretty_printer_1(A), pretty_printer_1(B)]);
-pretty_printer_1({plus, A, B}) -> 
-  io_lib:format("(~s + ~s)", [pretty_printer_1(A), pretty_printer_1(B)]).
+pretty_printer({num, Num}) -> io_lib:format("~B", [Num]);
+pretty_printer({minus, A, B}) -> 
+  io_lib:format("(~s - ~s)", [pretty_printer(A), pretty_printer(B)]);
+pretty_printer({plus, A, B}) -> 
+  io_lib:format("(~s + ~s)", [pretty_printer(A), pretty_printer(B)]).
